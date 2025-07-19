e87ff52b5c25bcdba71fa51f7dae6654144423b9
coachlou <124483397+coachlou@users.noreply.github.com>
2025-07-18 22:17:10 -0400

    Update start_openwebui.sh

--- start_openwebui.sh
+++ start_openwebui.sh
@@ -4,7 +4,7 @@ echo "Stopping and removing existing open-webui container..."
 docker stop open-webui && docker rm open-webui
 
 echo "Starting open-webui container with community sharing disabled..."
-# docker run -d -p 3000:8080 -e ENABLE_COMMUNITY_SHARING=false --add-host=host.docker.internal:host-gateway   -e OLLAMA_BASE_URL=http://localhost:11434 -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
+# docker run -d -p 3000:8080 -e ENABLE_COMMUNITY_SHARING=false --add-host=host.docker.internal:host-gateway   -e OLLAMA_BASE_URL=http://127.0.0.1:11434 -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
 docker compose -f docker-compose.macnative.yaml up -d 
 if [ $? -ne 0 ]; then
     echo "Failed to start the Open WebUI container."
